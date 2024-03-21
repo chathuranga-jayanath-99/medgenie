6dee4ed9d6e718069a4f5f6b88a43b936235645f
Chathuranga Jayanath <chathuranga.19@cse.mrt.ac.lk>
2024-03-21 08:31:10 +0530

    add print line to check values

--- fid_reader/train.py
+++ fid_reader/train.py
@@ -135,6 +135,7 @@ if __name__ == "__main__":
     options.add_reader_options()
     options.add_optim_options()
     opt = options.parse()
+    print(f"opt:{opt}")
 
     torch.manual_seed(opt.seed)
     init_distributed_mode(opt)
